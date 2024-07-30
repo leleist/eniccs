374ccf9a269934e10cab85d7e1b525a207c5b33d
leleist <l.leist@leist-design.de>
2024-07-30 15:54:25 +0200

    create class for handling hyperspectral EnMAP raster data
    includes loading method for EnMAP data from original directory structure and naming convention

---
+++ eniccs/hs_image.py
@@ -0,0 +1,31 @@
+import rasterio
+import glob
+import numpy as np
+print("hs_image.py loaded")
+class hs_image:
+    def __init__(self, dir_path, image_regEx=r"/*-SPECTRAL_IMAGE.TIF"):
+        self.image = None
+        self.dir_path = dir_path
+        self.image_path = glob.glob(dir_path + image_regEx)
+        self.image_regEx = image_regEx
+        self.profile = None
+        self.nodata = None
+        self.metadata = None
+        self.nodata_mask = None
+
+
+    def load_image(self):
+        with rasterio.open(self.image_path[0]) as src:
+            self.image = src.read()
+            self.profile = src.profile  #TODO: redunant with meta?
+            self.metadata = src.meta
+            self.nodata = src.nodata
+        return self
+
+    def get_no_data_mask(self):  # TODO: merge into land water mask?
+        no_data_condition = self.image[0, :, :] == self.nodata  # boolean mask for no data pixels
+        no_data_condition_inv = np.invert(no_data_condition)
+        no_data_mask = np.zeros(self.image.shape[1:])  # TODO: check if this is the right shape
+        self.nodata_mask = np.where(no_data_condition_inv, 1, no_data_mask)
+        return self
+
