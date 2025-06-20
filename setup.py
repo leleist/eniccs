6adc5052aff73ad86fedfed3de1645939049958a
leleist <l.leist@leist-design.de>
2025-06-20 18:35:22 +0200

    updated python and dependencies

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='eniccs',
-    version='0.18.6-alpha',
+    version='0.19.0-alpha',
     packages=['eniccs'],
     install_requires=[
-        'scikit-learn==1.3.2',
-        'rasterio==1.3.10',
-        'numpy==1.24.4',
-        'pandas==2.0.3',
-        'matplotlib==3.7.5',
-        'jupyter==1.0.0',
-        'scipy==1.10.1',
+        'scikit-learn>=1.7.0',
+        'rasterio>=1.4.3',
+        'numpy>=2.3.0',
+        'pandas>=2.3.0',
+        'matplotlib>=3.10.3',
+        'jupyter>=1.1.1',
+        'scipy>= 1.15.3',
+        'scikit-image >=0.25.0',
     ],
-    python_requires='>=3.8.19',
+    python_requires='>=3.11',
     description='Improvement of cloud and cloudshadow detection in hyperspectral EnMAP images',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',  # Format of the README file
@@ -24,6 +25,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
     ],
 )
