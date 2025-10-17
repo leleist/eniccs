9ea6f31f1a6c40f998c10ab6a17d8c8fc3a948a9
leleist <l.leist@leist-design.de>
2025-10-17 11:57:04 +0200

    resolved mutable default argument

--- eniccs/hs_image.py
+++ eniccs/hs_image.py
@@ -9,7 +9,10 @@ class HsImage:
     creates a convenient object from a hyperspectral image cube.
     """
 
-    def __init__(self, dir_path, filename_pattern='-SPECTRAL_IMAGE', extensions=['TIF', 'tif', 'TIFF', 'tiff', 'BSQ', 'bsq']):
+    def __init__(self, dir_path, filename_pattern='-SPECTRAL_IMAGE', extensions=None):
+        if extensions is None:
+            extensions = ['TIF', 'tif', 'TIFF', 'tiff', 'BSQ', 'bsq']
+
         self.image = None
         self.dir_path = dir_path
         self.image_path = []
@@ -18,24 +21,29 @@ class HsImage:
         self.profile = None
         self.no_data_value = None
 
-        # test trough different extensions
+        # test through different extensions
         for ext in extensions:
             self.image_path.extend(glob.glob(f"{dir_path}/*{filename_pattern}.{ext}"))
 
         if not self.image_path:
-            raise FileNotFoundError(f"No files found with pattern '*{filename_pattern}' and extensions {extensions}")
+            raise FileNotFoundError(f"No files found with pattern '*{filename_pattern}'"
+                                    f" and extensions {extensions}")
 
         # load image upon initialization
         self._load_image()
 
     def _load_image(self):
+        """
+        Load the hyperspectral image from file and set necessary attributes
+        """
         with rasterio.open(self.image_path[0]) as src:
             self.image = src.read()
             self.profile = src.profile
             self.no_data_value = src.nodata
 
     def plot_band(self, band=0):
-        """ Plot a band of the hyperspectral image
+        """
+        Plot a band of the hyperspectral image
         :param band: band ID to plot individual band
         """
         plt.imshow(self.image[band, :, :])
